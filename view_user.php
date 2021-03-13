b9a271efbce27ea2d82341ae9a36ecf911b4a0e4
mDollinar <m.dollinar@gmail.com>
2021-03-13 15:41:13 +0100

    fix

--- view_user.php
+++ view_user.php
@@ -13,79 +13,52 @@ else $photo = "file/personal_photos/".$photo;
 ?>
 
 <div id="maincontent">
-    <div class="container">
-     <br>
+
+<div class="container">
+    <br>
     <?php if($gest->checkSuperUser()){
         echo "<h1>".restring("Dati Utente&nbsp;<a href='crud_user.php?sec=op&id=".$id."'>%admin%</a>")."</h1>";
     }else{
         echo "<h1>Dati Utente</h1>";
     }?>
-     <br />
-        <div class="row row-utente justify-content-center">
-            <div class="col-3 border border-secondary photo"><h5><?php echo restring("Foto Tessera&nbsp;&nbsp;<a href='crud_user.php?sec=photo&id=".$id."'>%edit%</a>")?></h5><p><img src="<?php echo $photo ?>" title="Foto Tessera" class="photo fototessera"/></p>
-            </div>
-            <div class="col-6 border border-secondary asso">
-                <div class="row row-utente"> 
-                    <div class="col-5 asso">
-        <h5>Dati Associativi</h5>
-        <p>Numero Socio: <strong><?php echo printField($numero_socio) ?></strong></p>
-        <p>Data Iscrizione: <strong><?php echo printField($iscrizione[0]['giorno']); ?></strong></p>
-        <p>Data Dimissione: <?php echo printField($dimissione[0]['giorno']); ?></p>
-                    </div>
-        
-                    <div class="col-4 asso">
-
-    <?php if($gest->checkSuperUser()){
-        echo"
-        
-            <h5>Dati Gestionali</h5>
-            <p>Utente Master: ".printField($master)."</p>
-            <p>Operativo: ".printField($operativo)."</p>
-        
-        ";
-    }?>  
-                         
-                    </div>
-                </div>
-            </div>
-        </div>
-    <br>
-      
-        <div class="row row-utente">
-            <div class="col-9 border border-secondary" style="margin-left:20px">
-                <div class="row-row-utente">  
-                    <div class="col-4 anag"><h5><?php echo restring("Dati Anagrafici&nbsp;&nbsp;<a href='crud_user.php?sec=anag&id=".$id."'>%edit%</a>")?></h5><p>Cognome: <strong><?php echo $cognome?></strong></p><p>Nome: <strong><?php echo $nome?></strong></p><p>Codice Fiscale: <strong><?php echo $CF?></strong></p><p>Nato/a a <strong><?php echo $nascita_citta.", (".$nascita_pr.")"?></strong></p><p>il  <strong><?php echo printField($nascita)?></strong></p>
-                    </div>
-	                <div class="col-4 res">
-           
+    <br />
+    <div class="anag"><h4><?php echo restring("Foto Tessera&nbsp;&nbsp;<a href='crud_user.php?sec=photo&id=".$id."'>%edit%</a>")?></h4><p><img src="<?php echo $photo ?>" title="Foto Tessera" class="photo"/></p></div>
+    <div class="anag"><h4><?php echo restring("Dati Anagrafici&nbsp;&nbsp;<a href='crud_user.php?sec=anag&id=".$id."'>%edit%</a>")?></h4><p>Cognome: <?php echo $cognome?></p><p>Nome: <?php echo $nome?></p><p>Codice Fiscale:<?php echo $CF?></p><p>Nato/a a <?php echo $nascita_citta.", (".$nascita_pr.")"?> il <?php echo printField($nascita)?></p></div>
+	<div class="res">
         <?php
             if(strlen($domicilio)>0){
         ?>
-            <h5><?php echo restring("Residenza&nbsp;&nbsp;<a href='crud_user.php?sec=res&id=".$id."'>%edit%</a>")?></h5>
+            <h4><?php echo restring("Residenza&nbsp;&nbsp;<a href='crud_user.php?sec=res&id=".$id."'>%edit%</a>")?></h4>
         <?php
             }else{
         ?>
-            <h5><?php echo restring("Residenza e Domicilio&nbsp;&nbsp;<a href='crud_user.php?sec=resdom&id=".$id."'>%edit%</a>")?></h5>
+            <h4><?php echo restring("Residenza e Domicilio&nbsp;&nbsp;<a href='crud_user.php?sec=resdom&id=".$id."'>%edit%</a>")?></h4>
         <?php
             }
         ?>
-        <p><strong><?php echo $indirizzo.", ".$indirizzo_citta.", ".$indirizzo_cap." (".$indirizzo_pr.")"?></strong></p>
-                    </div>
+        <p><?php echo $indirizzo.", ".$indirizzo_citta.", ".$indirizzo_cap." (".$indirizzo_pr.")"?></p></div>
     <?php
         if(strlen($domicilio)>0){
     ?>
-    
-                    <div class="col-4 res"><h5><?php echo restring("Domicilio&nbsp;&nbsp;<a href='crud_user.php?sec=dom&id=".$id."'>%edit%</a>")?></h5><p><strong><?php echo $domicilio.", ".$domicilio_citta.", ".$domicilio_cap." (".$domicilio_pr.")"?></strong></p>
-                    </div>
+            <div class="dom"><h4><?php echo restring("Domicilio&nbsp;&nbsp;<a href='crud_user.php?sec=dom&id=".$id."'>%edit%</a>")?></h4><p><?php echo $domicilio.", ".$domicilio_citta.", ".$domicilio_cap." (".$domicilio_pr.")"?></p></div>
     <?php
         }
     ?>
-        
-                    <div class="col-4 contacts">
-                <h5><?php echo restring("Contatti&nbsp;&nbsp;<a href='crud_user.php?sec=cont&id=".$id."'>%edit%</a>")?></h5><p>Tel: <strong><?php echo $tel?></strong></p><p>Mail: <strong><?php echo $mail?></strong></p></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="contacts"><h4><?php echo restring("Contatti&nbsp;&nbsp;<a href='crud_user.php?sec=cont&id=".$id."'>%edit%</a>")?></h4><p>Tel: <?php echo $tel?></p><p>Mail: <?php echo $mail?></p></div>
+    <div class="asso">
+        <h4>Dati Associativi</h4>
+        <p>Numero Socio: <?php echo printField($numero_socio) ?></p>
+        <p>Data Iscrizione: <?php echo printField($iscrizione[0]['giorno']); ?></p>
+        <p>Data Dimissione: <?php echo printField($dimissione[0]['giorno']); ?></p>
     </div>
+    <?php if($gest->checkSuperUser()){
+        echo"
+        <div class='op'>
+            <h4>Dati Gestionali</h4>
+            <p>Utente Master: ".printField($master)."</p>
+            <p>Operativo: ".printField($operativo)."</p>
+        </div>
+        ";
+    }?>
+</div>
 </div>
